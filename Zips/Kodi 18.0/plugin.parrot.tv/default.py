d6be5df286576d85dba48e7f942fdda98fde4c5e
ParrotDevelopers <72994504+ParrotDevelopers@users.noreply.github.com>
2021-03-07 19:13:49 +0100

    Update default.py

--- Zips/Kodi 18.0/plugin.parrot.tv/default.py
+++ Zips/Kodi 18.0/plugin.parrot.tv/default.py
@@ -7,11 +7,13 @@ def CATEGORIES():
    addDir3('DE Channels','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Channels/DE%20Channels.txt',5,'https://i.ibb.co/6W5CN1r/DE.jpg','','')
    addDir3('CA Channels','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Channels/CA%20Channels.txt',6,'https://i.ibb.co/1b4HVfQ/CA.png','','')
    addDir3('US Channels','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Channels/US%20Channels.txt',7,'https://i.ibb.co/N3NqB1n/US.jpg','','')
-   addDir3('UK Channels','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Channels/UK%20Channels.txt',8,'https://i.ibb.co/gmzNmJt/UK.png ','','')   
-   addDir3('Futurama','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows/Futurama/Futurama.txt',9,'https://i.ibb.co/SBkYJ3m/Futurama.jpg','','')
-   addDir3('Movies','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/Movies/Movies.txt',10,'https://i.ibb.co/zbt3KHp/Movies.png','','')  
-   addDir3('Radio','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/Radio/Radio.txt',11,'https://i.ibb.co/J7k0ZYc/Radio.png','','')
-   addDir3('Check For Updates','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Updates/6.0.0/CheckForUpdate.txt',12,'https://i.ibb.co/GHdh2Pm/Check-For-Updates.png','','')
+   addDir3('UK Channels','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Channels/UK%20Channels.txt',8,'https://i.ibb.co/gmzNmJt/UK.png','','')
+   addDir3('Pluto TV','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/Pluto%20TV/Pluto%20TV.txt',9,'https://i.ibb.co/kg6vQHS/Pluto-TV.png','','')   
+   addDir3('The Simpsons','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows/The%20Simpsons/The%20Simpsons.txt',10,'https://i.ibb.co/61cKZcf/The-Simpsons.jpg','','')
+   addDir3('Futurama','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows/Futurama/Futurama.txt',11,'https://i.ibb.co/SBkYJ3m/Futurama.jpg','','')
+   addDir3('TV Shows','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows.txt',12,'https://i.ibb.co/pdgH08k/TV-Shows.png','','')
+   addDir3('Movies','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/Movies/Movies.txt',13,'https://i.ibb.co/zbt3KHp/Movies.png','','')  
+   addDir3('Check For Updates','https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Updates/8.0.0/CheckForUpdate.txt',14,'https://i.ibb.co/GHdh2Pm/Check-For-Updates.png','','')
 
 
 #################################################################################################################
@@ -54,9 +56,27 @@ def UK_Channels():
    match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
    for name,link, logo in match:
      addLink(name,link,logo,'','')
+     
+def Pluto_TV():
+   r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/Pluto%20TV/Pluto%20TV.txt')
+   match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
+   for name,link, logo in match:
+     addLink(name,link,logo,'','')
+     
+def The_Simpsons():
+   r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows/The%20Simpsons/The%20Simpsons.txt')
+   match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
+   for name,link, logo in match:
+     addLink(name,link,logo,'','')
         
 def Futurama():
    r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows/Futurama/Futurama.txt')
+   match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
+   for name,link, logo in match:
+     addLink(name,link,logo,'','')
+     
+def TV_Shows():
+   r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/TV%20Shows/TV%20Shows.txt')
    match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
    for name,link, logo in match:
      addLink(name,link,logo,'','')
@@ -67,14 +87,8 @@ def Movies():
    for name,link, logo in match:
      addLink(name,link,logo,'','')
      
-def Radio():
-   r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Special/Radio/Radio.txt')
-   match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
-   for name,link, logo in match:
-     addLink(name,link,logo,'','')
-     
 def Check_For_Updates():
-   r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Updates/6.0.0/CheckForUpdate.txt')
+   r = requests.get('https://raw.githubusercontent.com/ParrotDevelopers/Parrot-TV-Kodi/main/Updates/8.0.0/CheckForUpdate.txt')
    match = re.compile('name= (.+?) url= "(.+?)" logo= "(.+?)"').findall(r.content)
    for name,link, logo in match:
      addLink(name,link,logo,'','')
@@ -206,12 +220,16 @@ elif mode==7:
 elif mode==8:
         UK_Channels()
 elif mode==9:
-        Futurama()
+        Pluto_TV()
 elif mode==10:
-        Movies()
+        The_Simpsons()
 elif mode==11:
-        Radio()
+        Futurama()
 elif mode==12:
+        TV_Shows()
+elif mode==13:
+        Movies()
+elif mode==14:
         Check_For_Updates()
 
         
